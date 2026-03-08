299421b8e52eff3951feb1d6bdd11b2127fb12ba
Sjdjdbso <168467152+Sjdjdbso@users.noreply.github.com>
2026-03-08 20:19:03 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,111 @@
+# REQUIRED CONFIG
+BOT_TOKEN = "7992557881:AAE9PzLOV41uw1eEZDa3t0_-f_czetVzNpM"
+OWNER_ID = 7172158541
+TELEGRAM_API = 13470101
+TELEGRAM_HASH = "e8ab0629ef7ca0d20668527976d4b931"
+# OPTIONAL CONFIG
+TG_PROXY = {}
+USER_SESSION_STRING = ""
+CMD_SUFFIX = ""
+AUTHORIZED_CHATS = "-1002389245426 -1002814741350"
+SUDO_USERS = "7172158541"
+DATABASE_URL = "mongodb+srv://nazzawa09:nazzawa09@cluster0.umth574.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+STATUS_LIMIT = 4
+DEFAULT_UPLOAD = "gd"
+STATUS_UPDATE_INTERVAL = 10
+FILELION_API = ""
+STREAMWISH_API = ""
+EXCLUDED_EXTENSIONS = ""
+INCOMPLETE_TASK_NOTIFIER = False
+YT_DLP_OPTIONS = ""
+USE_SERVICE_ACCOUNTS = True
+NAME_SUBSTITUTE = ""
+FFMPEG_CMDS = {}
+UPLOAD_PATHS = {}
+# GDrive Tools
+GDRIVE_ID = "1LRMw4LclRtkuB9jXHljKdd74KuafMl6M"
+IS_TEAM_DRIVE = False
+STOP_DUPLICATE = True
+INDEX_URL = ""
+# Rclone
+RCLONE_PATH = ""
+RCLONE_FLAGS = ""
+RCLONE_SERVE_URL = ""
+RCLONE_SERVE_PORT = 0
+RCLONE_SERVE_USER = ""
+RCLONE_SERVE_PASS = ""
+# JDownloader
+JD_EMAIL = ""
+JD_PASS = ""
+# Sabnzbd
+USENET_SERVERS = [
+    {
+        "name": "main",
+        "host": "",
+        "port": 563,
+        "timeout": 60,
+        "username": "",
+        "password": "",
+        "connections": 8,
+        "ssl": 1,
+        "ssl_verify": 2,
+        "ssl_ciphers": "",
+        "enable": 1,
+        "required": 0,
+        "optional": 0,
+        "retention": 0,
+        "send_group": 0,
+        "priority": 0,
+    }
+]
+# Nzb search
+HYDRA_IP = ""
+HYDRA_API_KEY = ""
+# Update
+UPSTREAM_REPO = ""
+UPSTREAM_BRANCH = "master"
+# Leech
+LEECH_SPLIT_SIZE = 0
+AS_DOCUMENT = False
+EQUAL_SPLITS = False
+MEDIA_GROUP = False
+USER_TRANSMISSION = False
+HYBRID_LEECH = True
+LEECH_FILENAME_PREFIX = ""
+LEECH_DUMP_CHAT = "-1002389245426"
+THUMBNAIL_LAYOUT = ""
+# qBittorrent/Aria2c
+TORRENT_TIMEOUT = 0
+BASE_URL = ""
+BASE_URL_PORT = 0
+WEB_PINCODE = False
+# Queueing system
+QUEUE_ALL = 0
+QUEUE_DOWNLOAD = 0
+QUEUE_UPLOAD = 0
+# RSS
+RSS_DELAY = 600
+RSS_CHAT = ""
+RSS_SIZE_LIMIT = 0
+# Torrent Search
+SEARCH_API_LINK = ""
+SEARCH_LIMIT = 0
+SEARCH_PLUGINS = [
+    "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/piratebay.py",
+    "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/limetorrents.py",
+    "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/torlock.py",
+    "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/torrentscsv.py",
+    "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/eztv.py",
+    "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/torrentproject.py",
+    "https://raw.githubusercontent.com/MaurizioRicci/qBittorrent_search_engines/master/kickass_torrent.py",
+    "https://raw.githubusercontent.com/MaurizioRicci/qBittorrent_search_engines/master/yts_am.py",
+    "https://raw.githubusercontent.com/MadeOfMagicAndWires/qBit-plugins/master/engines/linuxtracker.py",
+    "https://raw.githubusercontent.com/MadeOfMagicAndWires/qBit-plugins/master/engines/nyaasi.py",
+    "https://raw.githubusercontent.com/LightDestory/qBittorrent-Search-Plugins/master/src/engines/ettv.py",
+    "https://raw.githubusercontent.com/LightDestory/qBittorrent-Search-Plugins/master/src/engines/glotorrents.py",
+    "https://raw.githubusercontent.com/LightDestory/qBittorrent-Search-Plugins/master/src/engines/thepiratebay.py",
+    "https://raw.githubusercontent.com/v1k45/1337x-qBittorrent-search-plugin/master/leetx.py",
+    "https://raw.githubusercontent.com/nindogo/qbtSearchScripts/master/magnetdl.py",
+    "https://raw.githubusercontent.com/msagca/qbittorrent_plugins/main/uniondht.py",
+    "https://raw.githubusercontent.com/khensolomon/leyts/master/yts.py",
+]
